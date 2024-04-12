a67826584a4178d3c3808b85714075ccc03e3bfc
peter279k <peter279k@gmail.com>
2024-04-12 11:23:44 +0800

    Remove other models

--- extract.php
+++ extract.php
@@ -8,11 +8,8 @@ if ($prompt === '') {
 
 $models = [
     '@cf/lykon/dreamshaper-8-lcm',
-    '@cf/runwayml/stable-diffusion-v1-5-img2img',
-    '@cf/runwayml/stable-diffusion-v1-5-inpainting',
-    '@cf/stabilityai/stable-diffusion-xl-base-1.0',
 ];
-$model = $models[random_int(0, count($models)-1)];
+$model = $models[0];
 
 $prompt = rawurldecode($prompt);
 $settingPath = 'settings.txt';
